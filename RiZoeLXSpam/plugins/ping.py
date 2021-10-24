b162a53896463b579cc97930c65862efbe74d0e5
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-10-24 20:26:43 +0530

    Update ping.py

--- RiZoeLXSpam/plugins/ping.py
+++ RiZoeLXSpam/plugins/ping.py
@@ -1,6 +1,7 @@
 
 
-from .. import Riz, Riz2, Riz3, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, SUDO_USERS
+from .. import Riz, Riz2, Riz3, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20
+from RiZoeLXSpam.config import SUDO_USERS
 from telethon import events
 from time import time
 from datetime import datetime
