8707bb1569e32e262742031f8ac6534070626048
RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-12 11:29:19 +0530

    UPDATE ALIVE

--- RiZoeLXSpam/plugins/alive.py
+++ RiZoeLXSpam/plugins/alive.py
@@ -13,7 +13,7 @@ RIZ_PIC = ALIVE_PIC
 @Riz.on(events.NewMessage(pattern=".alive"))
 async def alive(event):
     if event.sender_id in SMEX_USERS:
-     await e.reply(text, parse_mode=None, link_preview=None )
+     await event.reply(text, parse_mode=None, link_preview=None )
      await Riz.send_file(event.chat_id,
                                   RIZ_PIC,
                                   caption="✧ 𝑅𝐼𝑍𝑂𝐸𝐿 𝑋 𝑆𝑃𝐴𝑀 𝐼𝑍𝑍 𝐴𝐿𝐼𝑉𝐸𝐸 ✧\n\n ┏━━━━━━━━━━━━━━━━━━━\n ┣➣ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 3.9.6\n ┣➣ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 1.17\n ┣➣ sᴜᴘᴘᴏʀᴛ : [JOIN](https://t.me/DNHxHELL)\n ┣➣ ᴄʜᴀɴɴᴇʟ : [JOIN](https://t.me/RiZoeLX)\n ┗━━━━━━━━━━━━━━━━━━━\n\n 🖤 [𝐑𝐄𝐏𝐎](https://github.com/MrRizoel/RiZoeLXSpam) 🖤"                                
