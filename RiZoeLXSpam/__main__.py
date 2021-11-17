19a57b353091325fbab44c57ed770129d006a979
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-18 00:40:59 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -6,7 +6,6 @@ import glob
 from pathlib import Path
 from RiZoeLXSpam.utils import load_plugins
 import logging
-from . import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
