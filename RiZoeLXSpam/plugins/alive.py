546c9c6ab17f16ea93909cb0c5991d77c9d8830a
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-20 03:06:11 +0530

    UPDATE [alive.py]

--- RiZoeLXSpam/plugins/alive.py
+++ RiZoeLXSpam/plugins/alive.py
@@ -9,12 +9,30 @@ for x in SUDO_USERS:
     SMEX_USERS.append(x)
 
 RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
-            
+  
+
+          
+rizoel = "✧ 𝑅𝐼𝑍𝑂𝐸𝐿 𝑋 𝑆𝑃𝐴𝑀 𝐼𝑍𝑍 𝐴𝐿𝐼𝑉𝐸𝐸 ✧\n\n"
+
+rizoel += f"┏━━━━━━━━━━━━━━━━━━━\n"
+
+rizoel += f"┣➣ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : `3.9.6`\n"
+
+rizoel += f"┣➣ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : `1.17`\n"
+    
+rizoel += f"┣➣ sᴜᴘᴘᴏʀᴛ : [JOIN](https://t.me/DNHxHELL)\n"
+
+rizoel += f"┣➣ ᴄʜᴀɴɴᴇʟ : [JOIN](https://t.me/RiZoeLX)\n"
+
+rizoel += f"┗━━━━━━━━━━━━━━━━━━━\n\n"
+
+rizoel += f"🖤 [𝐑𝐄𝐏𝐎](https://github.com/MrRizoel/RiZoeLXSpam) 🖤"            
+                                    
 @Riz.on(events.NewMessage(pattern=".alive"))
 async def alive(event):
     if event.sender_id in SMEX_USERS:
      await event.reply(text, parse_mode=None, link_preview=None )
      await Riz.send_file(event.chat_id,
                                   RIZ_PIC,
-                                  caption="✧ 𝑅𝐼𝑍𝑂𝐸𝐿 𝑋 𝑆𝑃𝐴𝑀 𝐼𝑍𝑍 𝐴𝐿𝐼𝑉𝐸𝐸 ✧\n\n ┏━━━━━━━━━━━━━━━━━━━\n ┣➣ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 3.9.6\n ┣➣ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : 1.17\n ┣➣ sᴜᴘᴘᴏʀᴛ : [JOIN](https://t.me/DNHxHELL)\n ┣➣ ᴄʜᴀɴɴᴇʟ : [JOIN](https://t.me/RiZoeLX)\n ┗━━━━━━━━━━━━━━━━━━━\n\n 🖤 [𝐑𝐄𝐏𝐎](https://github.com/MrRizoel/RiZoeLXSpam) 🖤"                                
-                              )
+                                  caption=rizoel)
+    
