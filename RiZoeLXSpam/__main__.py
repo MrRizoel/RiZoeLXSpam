2ab47a28395f78847339e737e6c3b6399b7c1767
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-22 00:36:04 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -7,114 +7,12 @@ import glob
 from pathlib import Path
 from RiZoeLXSpam.utils import load_plugins
 import logging
-from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon import events
 from . import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-async def start_RiZoeLX():
-        try:
-            await Riz.start()
-            botme = await Riz.get_me()
-            await Riz(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz(functions.channels.JoinChannelRequest(channel="@DNHxHeLL"))
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz2.start()
-            await Riz2(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz2(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz2.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await  Riz3.start()
-            await Riz3(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz3(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz3.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz5.start()
-            await Riz5(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz5(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz5.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz4.start()
-            await Riz4(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz4(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz4.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz6.start()
-            await Riz6(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz6(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz6.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz7.start()
-            await Riz7(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz7(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz7.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz8.start()
-            await Riz8(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz8(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz8.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz9.start()
-            await Riz9(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz9(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz9.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
-        try:
-            await Riz10.start()
-            await Riz10(functions.channels.JoinChannelRequest(channel="@RiZoeLX"))
-            await Riz10(functions.channels.JoinChannelRequest(channel="@DNHxHELL"))
-            botme = await Riz10.get_me()
-            botid = telethon.utils.get_peer_id(botme)
-            SUDO_USERS.append(botid)
-        except Exception as e:
-            print(e)
-            pass
 
 path = "RiZoeLXSpam/plugins/*.py"
 files = glob.glob(path)
@@ -126,8 +24,6 @@ for name in files:
 
 print("RiZoeL Bot Spam Successfully deployed -!")
 print("Enjoy! Do visit @RiZoeLX")
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_RiZoeLX()) 
 
 if len(argv) not in (1, 3, 4):
     try:
