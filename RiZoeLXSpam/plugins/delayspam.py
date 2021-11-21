d1cd86f37027873957f689cf3489b2786188697a
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-22 01:43:18 +0530

    UPDATE [delayspam.py]

--- RiZoeLXSpam/plugins/delayspam.py
+++ RiZoeLXSpam/plugins/delayspam.py
@@ -19,7 +19,7 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from .. import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS
+from .. import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, SUDO_USERS
 
 SMEX_USERS = []
 for x in SUDO_USERS:
@@ -37,6 +37,16 @@ for x in SUDO_USERS:
 @Riz8.on(events.NewMessage(pattern=".delayspam"))
 @Riz9.on(events.NewMessage(pattern=".delayspam"))
 @Riz10.on(events.NewMessage(pattern=".delayspam"))
+@Riz11.on(events.NewMessage(pattern=".delayspam"))
+@Riz12.on(events.NewMessage(pattern=".delayspam"))
+@Riz13.on(events.NewMessage(pattern=".delayspam"))
+@Riz14.on(events.NewMessage(pattern=".delayspam"))
+@Riz15.on(events.NewMessage(pattern=".delayspam"))
+@Riz16.on(events.NewMessage(pattern=".delayspam"))
+@Riz17.on(events.NewMessage(pattern=".delayspam"))
+@Riz18.on(events.NewMessage(pattern=".delayspam"))
+@Riz19.on(events.NewMessage(pattern=".delayspam"))
+@Riz20.on(events.NewMessage(pattern=".delayspam"))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam <sleep time> <count> <message to spam>\n\n.delayspam <sleep time> <count> <reply to a message>\n\nCount and Sleeptime must be a integer."     
     if e.sender_id in SMEX_USERS:
