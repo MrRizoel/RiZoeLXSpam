47fa42839f9f4a29e2543ac92c2405f7c1b952c3
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-24 11:46:55 +0530

    Update bigspam.py

--- RiZoeLXSpam/plugins/bigspam.py
+++ RiZoeLXSpam/plugins/bigspam.py
@@ -48,7 +48,7 @@ for x in SUDO_USERS:
 @Riz19.on(events.NewMessage(pattern=r"\.bigspam"))
 @Riz20.on(events.NewMessage(pattern=r"\.bigspam"))
 async def spam(e):
-        usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
     error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
