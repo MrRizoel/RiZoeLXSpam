cc23f51ae24095f58bf0b30a9a841f53e675a1a4
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-24 11:45:50 +0530

    Update bigspam.py

--- RiZoeLXSpam/plugins/bigspam.py
+++ RiZoeLXSpam/plugins/bigspam.py
@@ -48,34 +48,31 @@ for x in SUDO_USERS:
 @Riz19.on(events.NewMessage(pattern=r"\.bigspam"))
 @Riz20.on(events.NewMessage(pattern=r"\.bigspam"))
 async def spam(e):
-        usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗕𝗶𝗴𝗦𝗽𝗮𝗺\n\nCommand:\n\n.bigspam <count> <message to spam>\n\n.bigspam <count> <reply to a message>\n\nCount must be a integer."
+        usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
+    error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
+    if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
-            return await e.reply(usage, parse_mode=None, link_preview=None)
-        Rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
-        if len(Rizoel) == 2:
-            message = str(Rizoel[1])
-            counter = int(Rizoel[0])
+        if len(rizoel) == 2:
+            message = str(rizoel[1])
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
+        elif e.reply_to_msg_id and smex.media:  
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
             for _ in range(counter):
-                async with e.client.action(e.chat_id, "typing"):
-                    if e.reply_to_msg_id:
-                        await smex.reply(message)
-                    else:
-                        await e.client.send_message(e.chat_id, message)
-                    await asyncio.sleep(0.1)
-        elif e.reply_to_msg_id and smex.media:
-            counter = int(Rizoel[0])
-            for _ in range(counter):
-                async with e.client.action(e.chat_id, "document"):
-                    smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
-                    await gifspam(e, smex)
-                await asyncio.sleep(0.1)
+                smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
+                await gifspam(e, smex)  
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(Rizoel[0])
-            for _ in range(counter):
-                async with e.client.action(e.chat_id, "typing"):
-                    await e.client.send_message(e.chat_id, message)
-                    await asyncio.sleep(0.1)
+            counter = int(rizoel[0])
+            if counter > 100:
+                return await e.reply(error, parse_mode=None, link_preview=None )
+            await asyncio.wait([e.respond(message) for i in range(counter)])
         else:
-            await e.reply(usage, parse_mode=None, link_preview=None)
+            await e.reply(usage, parse_mode=None, link_preview=None )
