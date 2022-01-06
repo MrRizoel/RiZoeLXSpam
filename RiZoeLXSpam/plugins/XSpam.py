b71641a77ee385e529a5ec60f651557984cb9e85
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-01-06 20:55:44 +0530

    Update XSpam.py

--- RiZoeLXSpam/plugins/XSpam.py
+++ RiZoeLXSpam/plugins/XSpam.py
@@ -19,7 +19,7 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from RiZoeLXSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, Riz21, Riz22, Riz23, Riz24, Riz25, Riz26, Riz27, Riz28, Riz29, Riz30, Riz31, Riz32, Riz33, Riz34, Riz35, Riz36, Riz37, Riz38, Riz39, Riz40, SUDO_USERS
+from RiZoeLXSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, Riz21, Riz22, Riz23, Riz24, Riz25, Riz26, Riz27, Riz28, Riz29, Riz30, Riz31, Riz32, Riz33, Riz34, Riz35, Riz36, Riz37, Riz38, Riz39, Riz40, OWNER_ID, DEV, SUDO_USERS
 from .. import CMD_HNDLR as hl
 from resources.data import RiZoeLX
 
@@ -69,7 +69,7 @@ from resources.data import RiZoeLX
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam <count> <message to spam>\n\n.spam <count> <reply to a message>\n\nCount must be a integer."
     error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
-    if e.sender_id in SUDO_USERS:
+    if e.sender_id in SUDO_USERS or e.sender_id in DEV:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
         Rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
@@ -142,7 +142,7 @@ async def spam(e):
 @Riz40.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam(?: |$)(.*)" % hl))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗕𝗶𝗴𝗦𝗽𝗮𝗺\n\nCommand:\n\n.bigspam <count> <message to spam>\n\n.bigspam <count> <reply to a message>\n\nCount must be a integer."
-    if e.sender_id in SUDO_USERS:
+    if e.sender_id in SUDO_USERS or e.sender_id in DEV:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
         rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
@@ -219,7 +219,7 @@ async def spam(e):
 @Riz40.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam(?: |$)(.*)" % hl))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n.delayspam <sleep time> <count> <message to spam>\n\n.delayspam <sleep time> <count> <reply to a message>\n\nCount and Sleeptime must be a integer."     
-    if e.sender_id in SUDO_USERS:
+    if e.sender_id in SUDO_USERS or e.sender_id in DEV:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
         smex = await e.get_reply_message()
@@ -299,7 +299,7 @@ async def spam(e):
 @Riz40.on(events.NewMessage(incoming=True, pattern=r"\%sdmspam(?: |$)(.*)" % hl))
 async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = **DM Spam**\n\nCommand:\n\n.dmspam <count> <username> <message to spam>\n\n.dmspam <count> <username> <reply to a message>\n\nCount must be a integer."
-    if e.sender_id in SUDO_USERS:
+    if e.sender_id in SUDO_USERS or e.sender_id in DEV:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
         rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 2)
@@ -312,6 +312,12 @@ async def spam(e):
             if int(g) in RiZoeLX:
                 text = f"I can't Dm To @RiZoeLX's Owner"
                 await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) == OWNER_ID:
+                text = f"This guy is Owner Of this Bots."
+                await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) in DEV:
+                text = f"This guy is a Full sudo user."
+                await e.reply(text, parse_mode=None, link_preview=None )
             elif int(g) in SUDO_USERS:
                 text = f"This guy is a sudo user."
                 await e.reply(text, parse_mode=None, link_preview=None )
@@ -322,13 +328,19 @@ async def spam(e):
                 for _ in range(counter):
                     async with e.client.action(g, "typing"):
                         await e.client.send_message(g, message)
-                        await asyncio.sleep(0.3)
+                        await asyncio.sleep(0.4)
         elif e.reply_to_msg_id and smex.media:
             user = str(Rizoelsexy[0])
             a = await e.client.get_entity(user)
             g = a.id
             if int(g) in RiZoeLX:
                 text = f"I can't Dm To @RiZoeLX's Owner"
+                await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) == OWNER_ID:
+                text = f"This guy is Owner Of this Bots."
+                await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) in DEV:
+                text = f"This guy is a Full sudo user."
                 await e.reply(text, parse_mode=None, link_preview=None )
             elif int(g) in SUDO_USERS:
                 text = f"This guy is a sudo user."
@@ -349,6 +361,12 @@ async def spam(e):
             if int(g) in RiZoeLX:
                 text = f"I can't Dm To @RiZoeLX's Owner"
                 await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) == OWNER_ID:
+                text = f"This guy is Owner Of this Bots."
+                await e.reply(text, parse_mode=None, link_preview=None )
+            elif int(g) in DEV:
+                text = f"This guy is a Full sudo user."
+                await e.reply(text, parse_mode=None, link_preview=None )
             elif int(g) in SUDO_USERS:
                 text = f"This guy is a sudo user."
                 await e.reply(text, parse_mode=None, link_preview=None )
@@ -358,7 +376,7 @@ async def spam(e):
                 for _ in range(counter):
                     async with e.client.action(g, "typing"):
                         await e.client.send_message(g, message)
-                        await asyncio.sleep(0.3)
+                        await asyncio.sleep(0.4)
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )       
 
