9b24c16e2aaab0ae950caf7054a56d46c1c1568c
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-01-06 21:28:03 +0530

    Update echo.py

--- RiZoeLXSpam/plugins/echo.py
+++ RiZoeLXSpam/plugins/echo.py
@@ -9,7 +9,7 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from RiZoeLXSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, Riz21, Riz22, Riz23, Riz24, Riz25, Riz26, Riz27, Riz28, Riz29, Riz30, Riz31, Riz32, Riz33, Riz34, Riz35, Riz36, Riz37, Riz38, Riz39, Riz40, SUDO_USERS, DEV, OWNER_ID
 
 from RiZoeLXSpam import CMD_HNDLR as hl
-from .sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
+from RiZoeLXSpam.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 from resources.data import RiZoeLX
 
 
