4a33c8f8beb48e41a17ded553a064517274f3fe9
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-01-07 13:17:56 +0530

    Create session.py

---
+++ session.py
@@ -0,0 +1,25 @@
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+
+print(
+    """┏━━┓━━┏━━━┓━━━━━━━━┏┓━\n┃┏┓┃┏┓┃┏━┓┃━━━━━━━━┃┃━\n┃┗┛┃┗┛┗┛━┛┃┏━━┓┏━━┓┃┃━\n┃━━┛┏┓┃┏━┏┓┃┏┓┃┃┏┓┃┃┃━\n┃┃┃┃┃┃┃┗━┛┃┃┗┛┃┃┃━┫┃┗━┓\n┗┛┗┛┗┛┗━━━┛┗━━┛┗━━┛┗━━┛"""
+)
+
+print("\n • Telethon Session •")
+
+print("\n\nEnter Your Valid Details To Continue!\n\n")
+
+        
+APP_ID = int(input("\nEnter APP ID here: "))
+API_HASH = input("\nEnter API HASH here: ")
+
+
+try:
+   with TelegramClient(StringSession(), APP_ID, API_HASH) as RiZoeL:
+       print(f"\nRiZoeL X Spam Session Givem Below\n\n{RiZoeL.session.save()}\n\nDon't Share This Anywhere")
+       print("\nSTRING SESSION GENERATE SUCCESSFULLY CHECK SAVED MASSAGE")
+       RiZoeL.send_message("me", f"**RiZoeL X Spam Session :**\n\n`{RiZoeL.session.save()}`\n\n__Do not share this anywhere!!__")
+       
+except Exception as e:
+    print(f"{e}") 
