f57bae484a576eed2f95595b514a152148534194
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-21 15:55:46 +0530

    Update __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -79,7 +79,6 @@ OWNER_ID = int(os.environ.get("OWNER_ID", None))
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 DEV.append(OWNER_ID)
-SUDO_USERS.append(OWNER_ID)
 
 # Sessions
 async def RiZoeLX():
